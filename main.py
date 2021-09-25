cf59aaa158e58be216606cc05a38cb33d36a4b5d
mustafaoguzbaran <=>
2021-09-25 23:15:33 +0300

    bmi calculation was done with python.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Python'da değişkenleri ve matematiksel operatörleri kullanarak vücut kitle indeksi hesaplama aracı
+#Python'da değişkenleri ve matematiksel operatörleri kullanarak vücut kitle indeksi hesaplama aracı.
 boy = input("Boy Uzunluğunuz (metre cinsinden 1.92 gibi): ")
 kilo = input("Kilonuz: (kilogram cinsinden): ")
 metrekare = float(boy)*float(boy)
